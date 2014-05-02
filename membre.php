84920309c4d625d3862f761b6d094b84961defbc
Valentin <valentblanchard@gmail.com>
2014-05-02 13:15:43 +0200

    v2

--- membre.php
+++ membre.php
@@ -9,19 +9,30 @@ if (!isset($_SESSION['login'])) {
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=1024, user-scalable=no">
 	<link rel='stylesheet' type='text/css' href='../style/style.css' />
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 	<meta name="viewport" content="initial-scale=1.0">
+	<link rel="stylesheet" media="screen and (max-width: 640px)" href="../style/scq.css" />
 	<title>Connexion</title>
 </head>
 <body>
-
+<br />
+<br />
+<div id="box1">
 <center>
-	<h1>
-	Connecté
-	</h1>
-	<a class="button" href='deconnexion.php'>Déconnexion</a>
-</center>
+	<?php
 
+	?>
+	<br />
+	<br />
+	<a class="button" href='ajouter.php'>Ajouter</a>
+	<br />
+	<br />
+	<a class="button" href='afficher.php'>Utilisation</a>
+	<br />
+	<br />
+	<a class="button" href='deconnexion.php'>DÃ©connexion</a>
+</center>
+</div>
 </body>
 </html>
